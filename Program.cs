595c0ccef645113458ff86eac86975025e57b580
joshmayeda <mayeda@sonoma.edu>
2023-04-18 00:33:16 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace DS_Scraper
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var html = await GetHtml();
+            var data = ParseHtmlUsingHtmlAgilityPack(html);
+            //Console.WriteLine("blah "+ data);
+        }
+
+        private static Task<string> GetHtml()
+        {
+            var client = new HttpClient();
+            return client.GetStringAsync("https://darksouls.wiki.fextralife.com/Daggers");
+        }
+
+        private static List<Weapon> ParseHtmlUsingHtmlAgilityPack(string html)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var items =
+                htmlDoc
+                    .DocumentNode
+                    .SelectNodes("//*[@id='wiki-content-block']/div/table/tbody/tr");
+
+            var size = items.Count - 2;
+
+            var data = new List<Weapon>(size);
+
+            for(var i = 2; i < items.Count; ++i){
+                var weapon = new Weapon();
+                // for(var j = 0; j < items[i].ChildNodes.Count; ++j){
+                //     Console.WriteLine("blah "+ items[i].ChildNodes[j].InnerText);
+                // }
+                Console.WriteLine("blah "+ items[i].InnerText);
+            }
+            return data;
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,27 @@
+using DS_Scraper;
+public class Weapon
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int AttackPower { get; set; }
+    public int PhysicalDamageReductionPercent { get; set; }
+    public int MagicAttackPower { get; set; }
+    public int MagicDamageReductionPercent { get; set; }
+    public int FireAttackPower { get; set; }
+    public int FireDamageReductionPercent { get; set; }
+    public int LightningAttackPower { get; set; }
+    public int LightningDamageReductionPercent { get; set; }
+    public int CriticalAttackPower { get; set; }
+    public int CriticalDamageReductionPercent { get; set; }
+    public int BleedDamage { get; set; }
+    public int PoisonDamage { get; set; }
+    public int DivineDamage { get; set; }
+    public int OccultDamage { get; set; }
+    public int RequiredStrength { get; set; }
+    public int RequiredDexterity { get; set; }
+    public int RequiredIntelligence { get; set; }
+    public int RequiredFaith { get; set; }
+    public int Durability { get; set; }
+    public int Weight { get; set; }
+    public string AttackType { get; set; }
+}
